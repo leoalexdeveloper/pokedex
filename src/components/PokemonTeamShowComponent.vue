9fa74c6c68e0294865881ec3e60e8515890276fb
leonardo.pereira <leonardo.pereira@dremio.com>
2022-05-14 21:30:56 -0300

    fix team index begin on 1

--- src/components/PokemonTeamShowComponent.vue
+++ src/components/PokemonTeamShowComponent.vue
@@ -3,7 +3,7 @@
     <div class="card col-lg-10 col-md-11 col-sm-12 my-4 p-2 m-auto bg-light d-flex flex-column justify-content-center">
       <div class="d-flex flex-row justify-content-evenly">
         <h1 class="col-2 p-0 fs-6 lead bg-primary p-0 text-light rounded-start d-flex align-items-center">
-          <p class="p-2 mt-3 rounded-end bg-light text-dark">Id: {{index}}</p>
+          <p class="p-2 mt-3 rounded-end bg-light text-dark">Id: {{index+1}}</p>
         </h1>
         <h1 class="col-3 p-2 fs-6 lead text-center bg-primary p-2 text-light rounded-0 d-flex align-items-center">
           Team: {{Utils.capsFirstLetter(team.name)}}
